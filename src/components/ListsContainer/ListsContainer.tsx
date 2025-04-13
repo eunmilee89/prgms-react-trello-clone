a7175c1cdc8b38674d36f0bf7899ee024c7babe2
Eunmi Lee <eunoia0235@gmail.com>
2025-04-14 03:02:57 +0900

    feat: ListsContainer 컴포넌트 추가

---
+++ src/components/ListsContainer/ListsContainer.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { IList } from '../../types';
+import ActionButton from '../ActionButton/ActionButton';
+import List from '../List/List';
+import { listsContainer } from './ListsContainer.css';
+
+type TListsContainerProps = {
+  lists: IList[];
+  boardId: string;
+};
+
+const ListsContainer: FC<TListsContainerProps> = ({ lists, boardId }) => {
+  return (
+    <div className={listsContainer}>
+      {lists.map((list) => (
+        <List key={list.listId} list={list} boardId={boardId} />
+      ))}
+      <ActionButton boardId={boardId} listId={''} list />
+    </div>
+  );
+};
+
+export default ListsContainer;
